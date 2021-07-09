4d47751528dc0578a6036dfafd64ef27cf2d2ff3
David Maltsev <luterfaeron@gmail.com>
2021-07-09 18:45:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfParityHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerLimitOfRange, uperLimitOfRange, countOfParity, countOfOddness;
+
+            Console.WriteLine("Введите нижнюю границу диапозона:");
+            lowerLimitOfRange = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите верхнюю границу диапозона");
+            uperLimitOfRange = int.Parse(Console.ReadLine());
+        }
+    }
+}
