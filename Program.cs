61d70f283f915a3e97054f1d54c7d8bb855eb30f
David Maltsev <luterfaeron@gmail.com>
2021-07-12 17:07:32 +0300

    Улучшение производительности.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace CountOfParityHW
     {
         static void Main(string[] args)
         {
-           int lowerLimitOfRange, uperLimitOfRange, resultOfOperation, countOfParity, countOfOdd, curentInteger;
-            countOfParity = 0;
+            int lowerLimitOfRange, uperLimitOfRange, curentInteger;
+            uint countOfEven, countOfOdd;
+            countOfEven = 0;
             countOfOdd = 0;
             lowerLimitOfRange = 0;
             uperLimitOfRange = 0;
@@ -31,17 +32,15 @@ namespace CountOfParityHW
 
             while (curentInteger <= uperLimitOfRange) 
             {
-                resultOfOperation = curentInteger % 2;
-
-                if (resultOfOperation == 0)
-                    countOfParity++;
+                if (curentInteger % 2 == 0)
+                    countOfEven++;
                 else
                     countOfOdd++;
 
                 curentInteger++;
              }
 
-            Console.WriteLine("Количество четных чисел в диапозоне равняется: " + countOfParity + " Количество нечетных чисел ровняется: " + countOfOdd);
+            Console.WriteLine("Количество четных чисел в диапозоне равняется: " + countOfEven + " Количество нечетных чисел ровняется: " + countOfOdd);
         }
     }
 }
